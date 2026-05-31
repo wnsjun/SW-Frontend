0c7dd0d4e2a44fa9af8d6d15c860281b6d4447f9
김준희 <shadd3338@gmail.com>
2026-05-31 13:48:03 +0900

    #11 [FEAT] 로그인/회원가입 API 연동

---
+++ src/api/auth.ts
@@ -0,0 +1,12 @@
+import { instance } from './instance';
+import type { LoginRequest, LoginResponse, SignupRequest, SignupResponse } from '../types/auth';
+
+export const postLogin = async (data: LoginRequest): Promise<LoginResponse> => {
+  const res = await instance.post<LoginResponse>('/api/v1/auth/login', data);
+  return res.data;
+};
+
+export const postSignup = async (data: SignupRequest): Promise<SignupResponse> => {
+  const res = await instance.post<SignupResponse>('/api/v1/auth/signup', data);
+  return res.data;
+};
