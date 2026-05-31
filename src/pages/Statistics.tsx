7cacdc722a9ea7c07203c1bcd481a560e44ff578
김준희 <shadd3338@gmail.com>
2026-05-31 23:26:26 +0900

    #19 [FEAT] Navbar 닉네임 localStorage에서 동적으로 반영

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -9,9 +9,11 @@ const MOCK_AI_MESSAGE =
   '이번 달 전반적인 습관 달성률이 지난 달보다 12% 향상되었어요! 특히 건강 카테고리의 꾸준한 실천이 돋보입니다. 학습 습관도 조금 더 신경 써보면 더욱 균형 잡힌 성장을 이룰 수 있을 것 같아요. 이 페이스를 유지해봐요! 💪';
 
 export default function Statistics() {
+  const nickname = localStorage.getItem('nickname') ?? '';
+
   return (
     <div className="min-h-screen bg-white pb-[100px]">
-      <Navbar variant="greeting" nickname="준희" />
+      <Navbar variant="greeting" nickname={nickname} />
 
       <div className="flex flex-col px-5">
         <div className="mt-[15px]">
